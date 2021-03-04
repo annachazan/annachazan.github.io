9f1886ae9bc31c2ed56c5ba76dc6e766505a2bed
annachazan <57268018+annachazan@users.noreply.github.com>
2021-03-04 03:01:59 -0300

    Add files via upload

---
+++ questao1.php
@@ -0,0 +1,23 @@
+<html>
+<head></head>
+<body>
+	<form action="resposta.php" method="post">
+		Questão 1: <br> <br>
+		Um gás ideal exerce pressão de 2 atm a 27°C. O gás sofre uma transformação isobárica na qual seu volume sofre um aumento de 20%. Supondo não haver alteração na massa do gás, sua temperatura passou a ser, em °C:
+
+		<br><br>Resposta:<br>
+		<input type="radio" name="resposta" value="A"> 32<br>
+		<input type="radio" name="resposta" value="B"> 100<br>
+		<input type="radio" name="resposta" value="C"> 54<br>
+		<input type="radio" name="resposta" value="D"> 120<br>
+		<input type="radio" name="resposta" value="E"> 87
+		
+<br><br>
+		
+
+		<input type="submit" value="Enviar">
+	</form>
+
+
+</body>
+</html>
---
+++ resposta.php
@@ -0,0 +1,17 @@
+<?php
+$resposta= $_POST['resposta'];
+
+echo "O gabarito é 87! No nosso exercício:
+
+T = Tk =  27 + 273
+
+T =Tk = 300 K
+
+Resolução:
+
+
+
+
+
+"
+?>
