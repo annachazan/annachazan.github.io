3c2af628bea98ca972a69fa8aee3bbb84d63c7cc
annachazan <57268018+annachazan@users.noreply.github.com>
2021-05-17 09:32:27 -0300

    Update vt2.js

--- vt2.js
+++ vt2.js
@@ -1,98 +1,77 @@
-<!DOCTYPE html>
-<html>
+$(function(){
 
-<head>
-    <meta charset="utf-8">
-    <title>JavaScript</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
-   
-    <link href="css/all.min.css" rel="stylesheet" />
-    <link href="css/templatemo-style.css" rel="stylesheet" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="icon" href="./icones/hh.png"> <link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet'>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
-    <link href="css/all.min.css" rel="stylesheet" />
-    <link href="css/templatemo-style.css" rel="stylesheet" />
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-       <script src="vt2.js"></script>
-  </head>
-<!--
-Simple House
-https://templatemo.com/tm-539-simple-house
--->
-<body> 
-
-    <div class="container">
-    <!-- Top box -->
-        <!-- Logo & Site Name -->
-        <div class="placeholder">
-            <div class="parallax-window" data-parallax="scroll" data-image-src="img/simple-house-01.jpg">
-                <div class="tm-header">
-                    <div class="row tm-header-inner">
-                        <div class="col-md-6 col-12">
-                            <img src="img/simple-house-logo.png" alt="Logo" class="tm-site-logo" /> 
-                            <div class="tm-site-text-box">
-                                <h1 class="tm-site-title">Java Script</h1>
-                                    
-                            </div>
-                        </div>
-                        <nav class="col-md-6 col-12 tm-nav">
-                            <ul class="tm-nav-ul">
-                                <li class="tm-nav-li"><a href="index.html" class="tm-nav-link">Home</a></li>
-                                <li class="tm-nav-li"><a href="about.html" class="tm-nav-link active">Java</a></li>
-                                <li class="tm-nav-li"><a href="deverquimica.html" class="tm-nav-link">Química</a></li>
-                                <li class="tm-nav-li"><a href="conjunto.html" class="tm-nav-link">Ambas</a></li>
-                            </ul>
-                        </nav>  
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <main>
-            <header>
-                   </header>
-        
-        
-        <div id="conteudo">
-                <h2 class="col-12 text-center tm-section-title">Exercícios</h2>
-                
-                    <nav>
-                
+    // receber os valores dos lados
     
-     
+    $("#botao").click(function(){
+        let l1 = $("#valor1").val();
+        let l2 = $("#valor2").val();
+        let l3 = $("#valor3").val();
         
+        console.log (l1);
         
-         
+        // verificar se os números são válidos 
         
- 
- <h3> <center> Digite os lados de um triângulo e veja sua classificação</h3><br>
-                          <center>  <input type="text" id="valor1" placeholder="Lado 1"><br><br>
-                          <center>  <input type="text" id="valor2" placeholder="Lado 2 "><br><br>
-                           <center>  <input type="text" id="valor3" placeholder="Lado 3"><br><br>
-                <center><button type="button" id="botao" class="btn">Enviar</button>
-                <button type="button" id="limpar" class="btn">Limpar</button>
-               <br> <br>
-             <center>   <p id="resultado"></p>
-            </div>
-    <a href="./bia.html"><i class="material-icons" style="color:#5E610B;"></i> Voltar para página original </a>
-    <br> <br> <br>
-            
+        if(l1<=0 || l2<=0 || l3<=0)
+        {
+            $("#resultado").text("Por favor, insira valores compativeis.");
+        }
+        else
+        {
+            $("#resultado").text("O seu triângulo é considerado " +
+             tr(l1,l2,l3));
+        }
+    })
 
+   
+// essa função verá se realmente é possível formar um triângulo com os valores fornecidos
+    function tr (l1, l2, l3)
+    {
+        l1 = parseFloat(l1);
+        l2 = parseFloat(l2);
+        l3 = parseFloat(l3);
+        if(existe(l1, l2, l3) == true)
+        {
+            return ct(l1, l2, l3);
+        }
+        else
+        {
+            return "inexistente, devido aos seus tamanhos.";
+        }
+    }
+    
+    
+      function existe (l1, l2, l3)
+    {
+        if(Math.abs(l2-l3) < l1 && l1 < (l2+l3)
+           || Math.abs(l1-l3) < l2 && l2 < (l1+l3)
+           || Math.abs(l1-l2) < l3 && l3 < (l1+l2))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+   
+    
+   
+    function ct (l1, l2, l3)
+    {
+        if(l1 == l2 && l1 == l3 && l2 == l3)
+        {
+            return "equilátero.";
+        }
+        if(l1 == l2 && l1!= l3 || l2 == l3 && l2 != l1 || l3 == l1 && l3 != l2)
+        {
+            return "isóceles.";
+        }
+        if(l1 != l2 && l1 != l3 && l3 != l2)
+        {
+            return "escaleno.";
+        }
 
-</div> 
-
-
-        <footer class="tm-footer text-center">
-            <p>Copyright &copy; 2020 Simple House 
-            
-            | Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
-        </footer>
-    </div>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/parallax.min.js"></script>
-</body>
-</html>
+    }
+    
+})
